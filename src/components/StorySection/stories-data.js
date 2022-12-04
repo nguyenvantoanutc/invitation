2d7b923921372fddac84ceb502dac937fd5da455
toannguyen10041996@gmail.com <toannguyen10041996@gmail.com>
2022-12-04 16:13:12 +0700

    - fix bug

--- src/components/StorySection/stories-data.js
+++ src/components/StorySection/stories-data.js
@@ -1,4 +1,3 @@
-import ImgNdodokLawang from '@assets/images/p-ndodok-lawang.jpg';
 import ImgFirstMeet from '@assets/images/s1.jpg';
 import ImgFirstGoOut from '@assets/images/s2.jpg';
 import ImgThree from '@assets/images/s3.jpg';
@@ -12,7 +11,7 @@ export const stories = [
         title: 'Ngày định mệnh',
         date: '08/08/2020',
         description:
-            'Ngưởi ta thường nói mùa thu là mùa thích hợp để bắt đầu 1 mối tình .Thành thật mà nói mùa nào cũng thích hợp cả. Chỉ cần anh đủ cam đảm đưa tay và em cũng sẵn sàng nắm lại là có một câu chuyện tình yêu. Nhưng thật ra, nếu được bắt đầu một tình yêu vào cái thời tiết dịu dàng, mát mẻ của mùa thu miền bắc thì vấn hơn mà, phải không? May mắn thay là duyên se trời định sắp xếp để chúng tôi gặp nhau ở đám cưới của 1 cô bạn thân. Vào một chiều thu, nhẹ nhàng, dịu dàng đến lạ. Khi mà ai cũng đẹp, rạng rỡ nhất. Có thể là với bạn chỉ là một ngày tuyệt vời để đi khám phá thế giới, đi chơi cùng bạn bè, người thân, ... Nhưng với chùng tôi đây là ngày đặc biệt, ngày chúng tôi gặp được người đặc biệt của nhau',
+            'Người ta thường nói mùa thu là mùa thích hợp để bắt đầu 1 mối tình .Thành thật mà nói mùa nào cũng thích hợp cả. Chỉ cần anh đủ cam đảm đưa tay và em cũng sẵn sàng nắm lại là có một câu chuyện tình yêu. Nhưng thật ra, nếu được bắt đầu một tình yêu vào cái thời tiết dịu dàng, mát mẻ của mùa thu miền bắc thì vấn hơn mà, phải không? May mắn thay là duyên se trời định sắp xếp để chúng tôi gặp nhau ở đám cưới của 1 cô bạn thân. Vào một chiều thu, nhẹ nhàng, dịu dàng đến lạ. Khi mà ai cũng đẹp, rạng rỡ nhất. Có thể là với bạn chỉ là một ngày tuyệt vời để đi khám phá thế giới, đi chơi cùng bạn bè, người thân, ... Nhưng với chùng tôi đây là ngày đặc biệt, ngày chúng tôi gặp được người đặc biệt của nhau',
         image: ImgFirstMeet,
     },
     {
@@ -24,13 +23,13 @@ export const stories = [
     },
     {
         title: 'Đón năm mới cùng nhau',
-        date: '29/01/2021',
+        date: '06/02/2021',
         description: 'Tết là khoảng thời gian mà những con người ở xa chỉ muốn trở về bên gia đình, những đúa trẻ mong được đi chơi, ăn uống và được tặng những phong bao lì xì đẹp đẽ với những lời chúc. Nhưng với những thanh niên đã đến tuổi cưới vợ, cưới chồng như chúng tôi, thật là không dễ dàng gì với những chuỗi câu hỏi mang sức nặng tinh thần mà không gì có thể cân đo đong đếm được như: "Cháu có ngưởi yêu chưa", "Cháu lương như thế nào", "Hôm nào cưới vậy cháu", ... Ai cũng muốn mình có đôi có cặp, cũng cần bàn tay sưởi ấm hay đơn giản là có một người đứng bên cạnh để gia đinh anh em đỡ hỏi hơn thôi mà. Nhưng mấy ai được đâu? À đấy chỉ là năm trc thôi chứ năm nay chúng tôi mạnh dạn hơn vì có người bên cạnh và rõng dạc nói rằng cháu có ngưởi yêu rồi ạ. Và những câu hỏi khác cũng không để ý nữa, hóa ra có ngưởi yêu lợi hại thật.',
         image: ImgThree,
     },
     {
         title: 'Yêu nhau giữa triều đại COVID',
-        date: '2018 - 2019',
+        date: '2021 - 2022',
         description:
             'Yêu xa mà lại gặp mùa dịch thì chắc khó lắm nhỉ? <br>' +
             'Tình yêu vẫn luôn là một thứ gì đó chẳng có ai có thể đoán trước được, rung động đẹp đẽ, hồi mới quen chúng tôi yêu nhau đang còn độ mặn nồng. Sau khoảng thời gian làm việc vất vã trong tuần thì lại vội vàng bắt chuyến xe sớm nhất để được cùng nhau đi ăn, lê la cafe, ... Nhưng mùa dịch bệnh thì những cái bình thưởng ấy lại cần khác đi một chút thì phải? Dịch khiến các tỉnh cũng bị giãn cách, những chuyến xe không được hoạt động nữa. Vì vậy những thói quen cũng phải thay đổi là tìm cho nhau những câu chuyện hay những trò chơi online, hay là vừa gọi vừa làm việc thay vì gặp. Và chẳng còn có những cuối tuần được gặp nhau. Đơn giản hơn chút nữa là không được dỗi hờn vu vơ. Cũng được không than thuở hay càm ràm những chuyện vụn vặt. Rồi cũng có lần giận dỗi nhìn nhau qua điện thoại mà bất lực. Những giọt nước mắt được giấu khi điện thoại vừa tắt. Là những tủi thân mà không nói thành lời. Những ấm ức không được giải bày. Có lẽ không thể phủ nhận rằng chính covid mang đến cho chúng tôi tìm và nhìn nhận lại bản thân. Đấy những lúc được gần nhau nó là cả trên sự tuyệt vời.',
@@ -40,12 +39,12 @@ export const stories = [
         title: 'Những gia vị của tình yêu',
         date: 'Everyday',
         description:
-            'Có những ngày chúng tôi giận nhau, khi hai đứa chẳng thể hiểu nhau, không đặt mình vào vị trí của đối phương để thông cảm nhau. Càng nói chuyện thì cái tôi của mỗi người lại càng lớn lên làm mọi chuyện thật sự căng thẳng và mệt mỏi. Cuối cùng đành phải chọn cách im lặng, mà im lặng thật sự là điều cả hai chẳng muốn. Có những câu chuyện tôi nói nhưng khiến chồng tôi rất buồn, thậm chí rất tổn thương, chán nản tới mức chẳng muốn nhìn thấy sự xuất hiện của tôi nữa, chẳng muốn nghe thêm những lời giải thích, chỉ bực tức biến mất đi, không có mặt trong cuộc sống của nhau cả ngày dài. Và có những khi chồng tôi để tôi chỉ một mình, bạn bè cảu tôi chẳng nhiều là mấy, vì từ lâu đã dành cho chồng tôi những điều quan trọng nhất, cứ thế chỉ có thể ngồi đợi chờ một cuộc điện thoại, một lời hỏi han quan tâm và cảm thấy nỗi nhớ toàn tràn ngập trong lòng. Rồi cũng vì nhớ mà cảm thấy rất cô đơn, rất cô đơn. Sau nhiều lần như vậy đã khiến cả hai cảm thấy cô đơn trong chính chuyện tình của mình. Những cuộc tranh luận xảy ra thường xuyên hơn. Cả hai đều mỏi mệt, chán nản và hụt hẫng để chỉ muốn nghĩ đến chuyện dừng lại. Nhưng kết thúc nó lại là những điều kỳ diệu, cuối cùng rồi tôi vẫn chọn không từ bỏ và bước chân lên xe ra Hà Nội để nói chuyện với chồng tôi. Chồng tôi miệng nói không quan tâm nhưng vẫn ầm thầm theo dõi hành trình chuyến xe dựa vào định vị icloud. Ngồi chờ vài tiếng, đi bộ dõi theo 7 km . Và cuối cùng cả hai vẫn chọn phấn đấu nhiều hơn nữa, vẫn chọn hi vọng cho mai sau. Chẳng để nhau, chẳng để thiếu vắng nhau trong đời.',
+            'Có những ngày chúng tôi giận nhau, khi hai đứa chẳng thể hiểu nhau, không đặt mình vào vị trí của đối phương để thông cảm nhau. Càng nói chuyện thì cái tôi của mỗi người lại càng lớn lên làm mọi chuyện thật sự căng thẳng và mệt mỏi. Cuối cùng đành phải chọn cách im lặng, mà im lặng thật sự là điều cả hai chẳng muốn. Có những câu chuyện tôi nói nhưng khiến chồng tôi rất buồn, thậm chí rất tổn thương, chán nản tới mức chẳng muốn nhìn thấy sự xuất hiện của tôi nữa, chẳng muốn nghe thêm những lời giải thích, chỉ bực tức biến mất đi, không có mặt trong cuộc sống của nhau cả ngày dài. Và có những khi chồng tôi để tôi chỉ một mình, bạn bè của tôi chẳng nhiều là mấy, vì từ lâu đã dành cho chồng tôi những điều quan trọng nhất, cứ thế chỉ có thể ngồi đợi chờ một cuộc điện thoại, một lời hỏi han quan tâm và cảm thấy nỗi nhớ người yêu tràn ngập trong lòng. Rồi cũng vì nhớ mà cảm thấy rất cô đơn, rất cô đơn. Sau nhiều lần như vậy đã khiến cả hai cảm thấy cô đơn trong chính chuyện tình của mình. Những cuộc tranh luận xảy ra thường xuyên hơn. Cả hai đều mỏi mệt, chán nản và hụt hẫng để chỉ muốn nghĩ đến chuyện dừng lại. Nhưng kết thúc nó lại là những điều kỳ diệu, cuối cùng rồi tôi vẫn chọn không từ bỏ và bước chân lên xe ra Hà Nội để nói chuyện với chồng tôi. Chồng tôi miệng nói không quan tâm nhưng vẫn ầm thầm theo dõi hành trình chuyến xe dựa vào định vị icloud. Ngồi chờ vài tiếng, đi bộ dõi theo 7 km . Và cuối cùng cả hai vẫn chọn phấn đấu nhiều hơn nữa, vẫn chọn hi vọng cho mai sau. Chẳng để nhau, chẳng để thiếu vắng nhau trong đời.',
         image: ImgSix,
     },
     {
         title: 'Bắt đầu những chặng đường mới',
-        date: '31 Agustus 2019',
+        date: '24/12/2022',
         description:
             'Có lẽ tới sau cùng, ai trong chúng ta, đều mong tìm một người cùng mình bước qua những giây phút cuối của cuộc đời. Ai cũng nói nên chọn cưới người yêu mình hay người mình yêu sẽ tốt hơn? Thật sự mà nói hôn nhân không phải thứ để lựa chọn mà là sự cảm mến, sự tin tưởng, sự đồng hành, sự hi sinh. Và hôn nhân là quá trình không phải là khoảnh khắc. Tôi có đọc được trong sách một câu mà tác giả đã bộc lộ quan điểm rằng: "Cưới xin không phải là chọn người tốt nhất trên đời để cưới mà chính là người đặc biệt nhất đối với mình”. Và chúng tôi đã gặp được người đặc biệt của nhau một cách nhẹ nhàng, không phô trương, chẳng ồn ào mà cũng không vội vã. Chẳng phải là ở cạnh nhau cả ngày chỉ cần là bình dị bên những câu hỏi thăm, cử chỉ ân cần, những hành động nhỏ nhắn và đôi khi chỉ là: “Ngày hôm nay của em thế nào?” Nếu mệt mỏi quá, thì cùng nhau thủ thỉ, tâm sự và xoa dịu sóng gió, giúp nhau "tẩy" đi những vết "nguệch ngoạc" trong bức tranh cuộc sống của đối phương. Để rồi, chỉ giữ lại những gam màu ấm áp. Để tận hưởng cuộc sống thú vị là cùng nhau nắm nắm tay đi dạo phố đông, la cà những góc phố, hẻm nhỏ, sẵn sàng xách xe đi lượn…... Dù lắm lúc, hai đứa chẳng biết là đi đâu. Mong rằng hiện tại hay mai sau chúng tôi sẽ nắm tay để cùng ăn, cùng chơi, cùng ngủ và cùng làm việc để khám phá cuộc sống thú vị này. ',
         image: ImgFive,
