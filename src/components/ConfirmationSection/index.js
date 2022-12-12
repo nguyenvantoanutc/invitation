cdf82e7abde144b4f330394199968c4f1ef4a969
toanutc <toannguyen10041996@gmail.com>
2022-12-12 08:30:12 +0700

    - update transport type

--- src/components/ConfirmationSection/index.js
+++ src/components/ConfirmationSection/index.js
@@ -71,9 +71,9 @@ function ConfirmationSection({guest}) {
                             <label>Hình thức di chuyển</label>
                             <select style={{backgroundColor: '#828282'}}
                                     className="form-control" placeholder="Transport" id="transport">
-                                <option value="-1">Có việc riêng, không thể tham gia được</option>
                                 <option value="0">Di chuyển cùng đoàn</option>
                                 <option value="1">Tự di chuyển</option>
+                                <option value="-1">Có việc riêng, không thể tham gia được</option>
                             </select>
                         </div>
                         <div className="form-group">
